c055f19639ea8e72849b2fe7426b33a4d5a8cc3d
Tyler Murphy <tjames7000@gmail.com>
2020-08-30 19:28:43 -0400

    Initial commit. The extension is working using browser alerts, which isn't ideal. There are a list of things to keep working on the readme.

---
+++ background.js
@@ -0,0 +1,79 @@
+/**
+ * References:
+ * - History: https://developer.chrome.com/extensions/history
+ * - Message Passing: https://developer.chrome.com/extensions/messaging
+ */
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if ('firstVisitTimeFor' in request) {
+		const domain = request.firstVisitTimeFor
+
+		// This fails to send a message if async/await is used, so use promise chains. See https://stackoverflow.com/a/56483156/3246022 for a little more information. This working solution was discovered through trial and error combined with that answer
+		getEarliestVisitTime(domain)
+			.then(firstVisitDate => {
+				const stringified = firstVisitDate ? firstVisitDate.toISOString() : "null"
+				console.debug(`sending response ${stringified}`)
+				sendResponse(stringified)
+			})
+
+		return true // return `true` to make sure the message channel stays open until the response is sent. See https://developer.chrome.com/extensions/runtime#event-onMessage for more details.
+	}
+
+	throw new Error(`Not sure how to handle request ${JSON.stringify(request)}`)
+})
+
+/**
+ * @param {string} domain
+ * @return {Promise<Date | null>}
+ */
+function getEarliestVisitTime(domain) {
+	console.debug(`finding history for ${domain}`)
+
+	return new Promise(resolve =>
+		// This search only returns the latest visit for each page, so after getting the latest, we'll need to get all visits.
+		chrome.history.search(
+			{
+				text: "",  // this will cause all history entries to be returned. Searching doesn't return all results that I'd expect it to, so get all results, and then filter them in this code.
+			},
+			async results => {
+				const withCorrectDomain = results.filter(result => result.url.startsWith(`http://${domain}`) || result.url.startsWith(`https://${domain}`))
+
+				console.debug(`found matching history results ${JSON.stringify(withCorrectDomain)}`)
+
+				if (withCorrectDomain.length === 0) {
+					return resolve(null)
+				}
+
+				const allVisits = flatten(await Promise.all(withCorrectDomain.map(({ url }) => getAllVisits(url))))
+				const earliestVisitTime = Math.min(...allVisits.map(visit => visit.visitTime))
+				const earliestVisit = new Date(earliestVisitTime)
+
+				console.debug(`found all visits: ${JSON.stringify(allVisits)}`)
+				console.debug(`earliest visit time: ${earliestVisit}`)
+
+				resolve(earliestVisit)
+			}
+		)
+	)
+}
+
+/**
+ * @param {string} url exactly as returned by the chrome history search response
+ * @return {Promise<Array<chrome.history.VisitItem>>}
+ */
+function getAllVisits(url) {
+	console.debug(`finding visits for ${url}`)
+
+	return new Promise(resolve => {
+		chrome.history.getVisits({ url }, resolve)
+	})
+}
+
+/**
+ * @template T
+ * @param {Array<Array<T>>} arrays
+ * @return {Array<T>}
+ */
+function flatten(arrays) {
+	return arrays.reduce((flattened, array) => flattened.concat(array), [])
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+### How it works
+
+`content.js` runs on all pages. It sends queries to the back-end, `background.js`, which has access to the browser's history API, and can tell `content.js` whether a page has been visited before. If the page hasn't been visited before, `content.js` displays a warning on the page.
+
+### Development
+
+Development resources:
+* Extension examples: https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples
+
+To do:
+* Add a closeable overlay to the site, rather than using `alert`s. This is partly because the alerts should be associated with the part of the page they go with. For example, if there's an iframe on the page, the alert for the iframe's domain should go in the iframe, because the top-level page may be a familiar page that doesn't need a warning.
+* Allow persistent warnings for a site as long as you've never entered data on the site
+* A mode in which warnings for a domain must be disabled explicitly to prevent them from showing up every time you use the site.
+
+### Credits
+
+Icons are from https://freeiconshop.com/icon/error-icon-flat/
